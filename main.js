c4179c32d216fd8ecbe854f3d45021437a09b137
Ilyes1 <ihannouch7@gmail.com>
2022-02-26 10:08:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 60) {
+            $('.home-nav, .mobile-menu').addClass('active')
+        } else {
+            $('.home-nav, .mobile-menu').removeClass('active')
+        }
+    })
+
+    $('.mobile-menu, .explore-mobile-menu, .earn-mobile-menu').hide()
+    $('.menu-btn').click(function() {
+        $('.mobile-menu, .explore-mobile-menu, .earn-mobile-menu').slideToggle(200)
+    })
+
+    $('.popup, .popup-invite').hide()
+    $('.profile-btn, .talk').click(function(e) {
+        e.preventDefault()
+        $('.popup').fadeIn()
+    })
+
+    $('.popup .popup-overlay').click(function() {
+        $('.popup').fadeOut()
+    })
+
+    $('.welcome-btn').click(function(e) {
+        e.preventDefault()
+        $('.popup-invite').fadeIn()
+    })
+
+    $('.popup-invite .popup-overlay').click(function() {
+        $('.popup-invite').fadeOut()
+    })
+
+
+    $('.filter').click(function() {
+        $('.filter').not(this).find('ul').slideUp()
+        $(this).find('ul').slideToggle(200)
+    })
+
+
+})
